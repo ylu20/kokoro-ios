75982a16b1f06c2147d531a40abe92a0cfe2b022
ylu20 <ylu20.cv@gmail.com>
2026-03-16 12:12:23 -0400

    Update package dependencies to use 'from' syntax

--- Package.swift
+++ Package.swift
@@ -16,10 +16,10 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/ml-explore/mlx-swift", exact: "0.30.2"),
+    .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.30.2"),
     // .package(url: "https://github.com/mlalma/eSpeakNGSwift", from: "1.0.1"),
-    .package(url: "https://github.com/mlalma/MisakiSwift", exact: "1.0.6"),
-    .package(url: "https://github.com/mlalma/MLXUtilsLibrary.git", exact: "0.0.6")
+    .package(url: "https://github.com/mlalma/MisakiSwift", from: "1.0.6"),
+    .package(url: "https://github.com/mlalma/MLXUtilsLibrary.git", from: "0.0.6")
   ],
   targets: [
     .target(
